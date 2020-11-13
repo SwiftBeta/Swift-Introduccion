3fa4d940da586ff56d3927df2b6baf0694e8fd91
SwiftBeta <swiftbeta.blog@gmail.com>
2020-11-13 11:42:29 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Swift-Introduccion
+Aprende Swift. Lee los posts y mira el código de ejemplo directamente de [swiftbeta.com](www.swiftbeta.com) o de los Playgrounds
+
+1. Playgrounds - https://www.swiftbeta.com/playgrounds/
+2. Constantes y Variables - https://www.swiftbeta.com/constantes-y-variables-en-swift/
+3. Opcionales - https://www.swiftbeta.com/opcionales-en-swift/
+4. Colecciones: Array, Set y Dictionary - https://www.swiftbeta.com/colecciones-array-set-y-dictionary-en-swift/
+5. Control flow: For, While, If, Guard, Switch https://www.swiftbeta.com/control-flow-en-swift/
+6. Funciones - https://www.swiftbeta.com/funciones-en-swift/
+7. Class y Struct - https://www.swiftbeta.com/class-y-struct-en-swift/
+8. Propiedades - https://www.swiftbeta.com/propiedades-en-swift/
+9. Métodos - https://www.swiftbeta.com/metodos-en-swift/
+10. Closures - https://www.swiftbeta.com/closures-en-swift/
+11. Enums - https://www.swiftbeta.com/enums-en-swift/
+12. Herencia - https://www.swiftbeta.com/herencia-en-swift/
+13. Automatic Reference Counting - https://www.swiftbeta.com/automatic-reference-counting-en-swift/
+14. Protocolos - https://www.swiftbeta.com/protocolos-en-swift/
---
+++ Swift Introducción.playground/Pages/Inicializadores.xcplaygroundpage/Contents.swift
@@ -0,0 +1,114 @@
+//: [Previous](@previous)
+import Foundation
+
+/*:
+ # Inicializadores
+  Podéis encontrar el post aquí https://www.swiftbeta.com/inicializacion-en-swift/
+*/
+
+
+/*:
+ ## Inicializadores
+*/
+
+struct User {
+    var name: String
+    
+    init() {
+        name = "SwiftBeta"
+    }
+}
+
+var user = User.init()
+print("The default user is \(user.name)")
+
+/*:
+ ## Propiedades con valores por defecto
+*/
+
+struct Coworker {
+    var name = "SwiftBeta"
+}
+
+let coworker = Coworker()
+print("The default coworker is \(coworker.name)")
+
+
+/*:
+ ## Personalizar la iniciación
+ ### Parámetros de inicialización
+*/
+
+struct Database {
+    var name: String
+    
+    init(withName name: String) {
+        self.name = name
+    }
+    
+    init(_ name: String) {
+        self.name = name
+    }
+}
+
+let database = Database(withName: "mobile")
+let mobileDatabase = Database("mobile")
+
+
+/*:
+ ## Delegar la inicialización
+*/
+
+struct City {
+    let name: String
+    let countries: [String]
+    
+    init(name: String, countries: [String]) {
+        self.name = name
+        self.countries = countries
+    }
+    
+    init(name: String) {
+        self.init(name: name, countries: [])
+    }
+}
+
+let city = City(name: "New York")
+
+/*:
+ ## Ejemplos Designated y Convenience Initializers
+*/
+
+class Food {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    convenience init() {
+        self.init(name: "[Unnamed]")
+    }
+}
+
+let food = Food()
+let foodWithName = Food(name: "Bacon")
+
+class RecipeIngredient: Food {
+    var quantity: Int
+    
+    init(name: String, quantity: Int) {
+        self.quantity = quantity
+        super.init(name: name)
+    }
+    
+    override convenience init(name: String) {
+        self.init(name: name, quantity: 1)
+    }
+}
+
+let oneMysteryItem = RecipeIngredient()
+let oneBacon = RecipeIngredient(name: "Bacon")
+let sixEggs = RecipeIngredient(name: "Eggs", quantity: 6)
+
+//: [Next](@next)
